45b01fbdb57c5a4281d6abdbd04dffdc3e7aab1e
Cleverton <clever.nvs@gmail.com>
2020-12-16 00:22:01 -0300

    feat: implmentação do add To Cart

--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -1,13 +1,37 @@
-import { AppBar, Button, Toolbar, Typography } from '@material-ui/core';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { AppBar, Badge, Button, Toolbar, Typography } from '@material-ui/core';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
 
 export default function NavBar() {
+
+  const cart = useSelector(state => state.cart);
+  const { cartItems } = cart;
+
   return (
     <div>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6">AMAZONA</Typography>
-          <Button color="inherit">Login</Button>
+          <Link to={'/'}>
+            <Typography variant="h6">AMAZONA</Typography>
+          </Link>
+          <Link to={'/cart'}>
+            {
+              cartItems.length > 0 ? (
+                <Badge badgeContent={cartItems.length} color="secondary">
+                  <ShoppingCartIcon />
+                </Badge>
+              ) : (
+                  <Badge badgeContent={0} color="secondary">
+                    <ShoppingCartIcon />
+                  </Badge>
+                )
+            }
+          </Link>
+          <Link to={'/signin'}>
+            <Button color="inherit">Sign In</Button>
+          </Link>
         </Toolbar>
       </AppBar>
     </div >
--- frontend/src/components/Product/index.js
+++ frontend/src/components/Product/index.js
@@ -7,18 +7,23 @@ import {
   CardMedia,
   Typography
 } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 export default function Product(props) {
 
   const { product } = props;
 
   return (
-    <Card>
+    <Card key={product._id}>
       <CardActionArea>
-        <CardMedia image={`product/${product._id}`} title={product.name} />
+        <Link to={`product/${product._id}`}>
+          <CardMedia image={`product/${product._id}`} title={product.name} />
+        </Link>
         <CardContent>
           <Rating rating={product.rating} numReviews={product.numReviews} />
-          <Typography variant="h5" gutterBottom>{product.name}</Typography>
+          <Link to={`product/${product._id}`}>
+            <Typography variant="h5" gutterBottom>{product.name}</Typography>
+          </Link>
           <Typography variant="body2" gutterBottom>{product.price}</Typography>
         </CardContent>
       </CardActionArea>
