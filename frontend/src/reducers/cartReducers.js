e3a72367e7e5bd499d29fb4c7a94763bb519cab9
Cleverton <clever.nvs@gmail.com>
2020-12-17 08:24:40 -0300

    feat: implementação excluir produto do carrinho

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
@@ -6,16 +6,12 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
       const item = action.payload;
       const existItem = state.cartItems.find((x) => x.product === item.product);
       if (existItem) {
-        return {
-          ...state,
-          cartItems: state.cartItems.map((x) => x.product === existItem.product ? item : x),
-        };
+        return { ...state, cartItems: state.cartItems.map((x) => x.product === existItem.product ? item : x) };
       } else {
-        return {
-          ...state,
-          cartItems: [...state.cartItems, item]
-        };
-      }
+        return { ...state, cartItems: [...state.cartItems, item] };
+      };
+    case CART_REMOVE_ITEM:
+      return { ...state, cartItems: state.cartItems.filter(x => x.product !== action.payload) };
     default:
       return state;
   }
