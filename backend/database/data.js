8bf00827cc7529fbd2f9b9a4f51c87c0739779ef
Cleverton <clever.nvs@gmail.com>
2020-12-17 09:53:56 -0300

    feat: conectando com o BD

--- backend/database/data.js
+++ backend/database/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Adminum',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456', 8),
+      isAdmin: true
+    },
+    {
+      name: 'Userum',
+      email: 'user@example.com',
+      password: bcrypt.hashAsync('123456', 8),
+      isAdmin: false
+    },
+  ],
   product: [
     {
       _id: '1',
